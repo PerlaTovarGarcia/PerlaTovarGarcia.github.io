b6c2c2a3389857b65400cdead70f6e61311b4699
Perla <pataditaenloshuevos@gmail.com>
2019-06-18 08:44:03 -0500

    Updates

---
+++ precache-manifest.110df664807212ee122c23f1ab7b8c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c1ee49d2b8a87b024520ad00e9281a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7943fa7e03a42c03e011",
+    "url": "/static/css/main.347a36eb.chunk.css"
+  },
+  {
+    "revision": "e9cb67d4a2b2a4d157dd",
+    "url": "/static/js/2.f3898a27.chunk.js"
+  },
+  {
+    "revision": "7943fa7e03a42c03e011",
+    "url": "/static/js/main.08e42e30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad87c55137e2fbae1cec2bbb9baf9490",
+    "url": "/static/media/globos.ad87c551.png"
+  },
+  {
+    "revision": "2c52f7c413986928cdf29bb9464e210f",
+    "url": "/static/media/meketrefe.2c52f7c4.png"
+  }
+]);
